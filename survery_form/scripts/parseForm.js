9c9b1068b26bdf06581d0aecb7e278f2d57423fe
Reins Janovskis <reinis.janovskis@hotmail.com>
2024-05-18 12:30:08 +0100

    Added javascript on form submit

---
+++ survery_form/scripts/parseForm.js
@@ -0,0 +1,15 @@
+function handleSubmit(e) {
+  e.preventDefault();
+  const nameValue = document.getElementById("name").value;
+  alert(nameValue);
+}
+
+function sayHello(e) {
+  alert("Hello!");
+  e.preventDefault();
+}
+
+const form = document.getElementById("survey-form");
+form.addEventListener("submit", handleSubmit);
+
+//document.getElementById("submit").addEventListener("click", sayHello);
